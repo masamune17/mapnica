2928597d19c5d85b12c4d6a95bf50842593bf7fd
masamune17 <2396niisankyumu@gmail.com>
2021-09-28 08:07:44 +0900

    リファクタリング

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,5 @@
 class MapsController < ApplicationController
   def index
     @all_history_position = History.all.pluck(:latitude, :longitude).to_json
-    logger.debug("コントローラーでデバッグだよ")
-    logger.debug(@all_history_position)
   end
 end
