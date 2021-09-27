81015c8d32d9a80bd60452e4a775ef4df34c5932
masamune17 <2396niisankyumu@gmail.com>
2021-09-28 07:17:53 +0900

    google map にdb内ののデータの位置をマーカー表示する

--- app/assets/javascripts/google-map-api.js
+++ app/assets/javascripts/google-map-api.js
@@ -1,6 +1,29 @@
-function initMap(){
-  let map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 35.4036, lng: 139.4553},
+function initMap() {
+const marker = [];
+const markerData = JSON.parse(document.querySelector("#marker-data").dataset.position);
+console.log(markerData)
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  center: {
+    lat: markerData[0][0],
+    lng: markerData[0][1]},
   zoom: 4
   });
+  
+    for (var i = 0; i < markerData.length; i++) {
+      markerLatLng = new google.maps.LatLng({
+        lat: markerData[i][0],
+        lng: markerData[i][1]
+      });
+
+      marker[i] = new google.maps.Marker({
+        position: markerLatLng,
+        map: map
+      });
+    }
 }
+
+window.onload = function() {
+  initMap();
+}
+
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,7 @@
 class MapsController < ApplicationController
   def index
+    @all_history_position = History.all.pluck(:latitude, :longitude).to_json
+    logger.debug("コントローラーでデバッグだよ")
+    logger.debug(@all_history_position)
   end
 end
