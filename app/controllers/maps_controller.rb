b78cd84f0d947a94b59edebf3124cb2d253cedc5
masamune17 <2396niisankyumu@gmail.com>
2021-10-03 12:08:07 +0900

    DBにcsvファイルからデータを入れられるようにする
    年代の検索でバグがあったので修正（紀元前がうまく検索できていなかった）
    labelが英語の場合にエラーが出たので修正

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,11 +2,15 @@
 
 class MapsController < ApplicationController
   def index
-    all_history_abstract = History.all.pluck(:abstract)
-    @all_history_abstract = all_history_abstract.map{|a| a.gsub(/\s/, "　")}.to_json
-    @all_history_position = History.all.pluck(:latitude, :longitude, :accrual_date, :label).to_json
+    # all_history_label = History.all.pluck(:label)
+    # @all_history_label = all_history_label.map{|a| a.gsub(/\s/, "　")}.to_json
+
+    all_history_data = History.all.pluck(:label, :abstract)
+    @all_history_data = all_history_data.map{|a| a.map{|a| a.gsub(/\s/, "　")}}.to_json
+
+    @all_history_position = History.all.pluck(:latitude, :longitude, :accrual_date).to_json
     logger.debug("コントローラーでデバッグだよ")
-    logger.debug(@all_history_abstract)
+    logger.debug(@all_history_data)
     logger.debug(@all_history_position.class)
   end
 end
