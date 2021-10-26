65b7a1a882914a641250a115b4a5996ca523cb52
masamune17 <2396niisankyumu@gmail.com>
2021-10-26 12:27:37 +0900

    searche_year_spec.rb 作成

--- spec/requests/api/maps/searche_year_spec.rb
+++ spec/requests/api/maps/searche_year_spec.rb
@@ -1,18 +1,19 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-RSpec.describe 'Api::Maps::SearcheWord', type: :request do
+RSpec.describe 'Api::Maps::SearcheYear', type: :request do
+  before do
+    @map1 = create(:one)
+    @map2 = create(:two)
+    @map3 = create(:three)
+    @map4 = create(:four)
+  end
   it '特定の年代のデータを取得する' do
     era = 1000
     uri = '/api/maps/searche_year'
-
     get "#{uri}?era=#{era}"
     json = JSON.parse(response.body)
-
-    # リクエスト成功を表す200が返ってきたか確認する。
     expect(response.status).to eq(200)
-
-    # 正しい数のデータが返されたか確認する。
     expect(json.length).to eq(2)
   end
 end
