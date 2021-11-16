2f620c0795f732f3858c18df01bae84060e0ff3b
masamune17 <2396niisankyumu@gmail.com>
2021-11-17 04:44:49 +0900

    OGPを設定

---
+++ app/helpers/meta_tags_helper.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module MetaTagsHelper
+  # rubocop:disable Metrics/MethodLength
+  def default_meta_tags
+    {
+      site: 'Mapnica',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'Mapnicaは歴史の出来事を地図上に表示するサービスです',
+      viewport: 'width=device-width, initial-scale=1.0',
+      og: {
+        title: :title,
+        type: 'website',
+        site_name: 'Mapnica',
+        description: :description,
+        image: 'http://localhost:3000/ogp/ogp.png',
+        url: 'http://localhost:3000'
+      },
+      twitter: {
+        card: 'summary',
+        site: '@Masamune_catcat',
+        description: :description,
+        image: 'http://localhost:3000/ogp/ogp.png',
+        domain: 'http://localhost:3000'
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
+end
