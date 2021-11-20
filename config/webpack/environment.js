c900ed3928a3c6f966eecf211fe7bb3c5d0ed0d2
masamune17 <2396niisankyumu@gmail.com>
2021-11-20 15:45:32 +0900

    vue3をインストール

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
+const { VueLoaderPlugin } = require('vue-loader')
+environment.plugins.prepend(
+  'VueLoaderPlugin',
+  new VueLoaderPlugin()
+)
+environment.loaders.prepend('vue', {
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
+  }]
+})
+
 module.exports = environment
