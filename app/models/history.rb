50bf5d2ab3800d62812db3bdad2a9f741d5418a3
masamune17 <2396niisankyumu@gmail.com>
2021-12-13 17:40:26 +0900

    HistoryモデルにValidationを追加

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class History < ApplicationRecord
+  validates :label, presence: true
+  validates :abstract, presence: true
+  validates :latitude, presence: true, numericality: true
+  validates :longitude, presence: true, numericality: true
+  validates :accrual_date, presence: true
 end
